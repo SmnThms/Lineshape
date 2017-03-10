deaf2a5eca9ad29e350a7d29c0bac1784a1cf89c
SmnThms <simon.thomas8@gmail.com>
2017-03-10 12:34:07 +0100

    Add files via upload

---
+++ fluo3S_H_mars2017_I.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 12:13:05 2017
+
+@author: Simon
+"""
+
+from __future__ import division
+import numpy as np
+from scipy.misc import factorial
+
+
+##### STRUCTURE DU PROGRAMME #####
+# 1. Définition des bases et matrices de passage
+# 2. Définition des hamiltoniens
+# 3. Résolution des équations de Bloch optiques pour chaque sous-niveau
+# 4. Calcul de la forme de raie finale et enregistrement des valeurs
+# 5. Ajustement par une lorentzienne et enregistrement des valeurs
+
+
+##### 1. Définition des bases et matrices de passage
+
+class Niveau:
+    def __init__(self,S=False, L=False, I=False, J=False, F=False, mS=False, mL=False, mI=False, mJ=False, mF=False):
+        self.S, self.L, self.I, self.J, self.F = S, L, I, J, F
+        self.mS, self.mL, self.mI, self.mJ, self.mF = mS, mL, mI, mJ, mF
+
+def LmSmLmI():
+    LmSmLmI = []
+    LmSmLmI.append(Niveau( L=1, mS=1/2,  mL=1,  mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=1/2,  mL=1,  mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=1/2,  mL=0,  mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=-1/2, mL=1,  mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=1/2,  mL=0,  mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=1/2,  mL=-1, mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=-1/2, mL=1,  mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=-1/2, mL=0,  mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=1/2,  mL=-1, mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=-1/2, mL=0,  mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=-1/2, mL=-1, mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=1, mS=-1/2, mL=-1, mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=0, mS=1/2,  mL=0,  mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=0, mS=1/2,  mL=0,  mI=-1/2, S=1/2 ))
+    LmSmLmI.append(Niveau( L=0, mS=-1/2, mL=0,  mI=1/2,  S=1/2 ))
+    LmSmLmI.append(Niveau( L=0, mS=-1/2, mL=0,  mI=-1/2, S=1/2 ))
+    return LmSmLmI
+
+def LJmJmI():
+    LJmJmI = []
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=3/2,  mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=3/2,  mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=1/2,  mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=1/2,  mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=-1/2, mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=-1/2, mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=-3/2, mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=3/2, mJ=-3/2, mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=1/2, mJ=1/2,  mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=1/2, mJ=1/2,  mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=1/2, mJ=-1/2, mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=1, J=1/2, mJ=-1/2, mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=0, J=1/2, mJ=1/2,  mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=0, J=1/2, mJ=1/2,  mI=-1/2, I=1/2 ))
+    LJmJmI.append(Niveau( L=0, J=1/2, mJ=-1/2, mI=1/2,  I=1/2 ))
+    LJmJmI.append(Niveau( L=0, J=1/2, mJ=-1/2, mI=-1/2, I=1/2 ))
+    return LJmJmI
+    
+def LJFmF():    
+    LJFmF = []
+    LJFmF.append(Niveau( L=1, J=3/2, F=2, mF=2  ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=2, mF=1  ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=1, mF=1  ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=2, mF=0  ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=1, mF=0  ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=2, mF=-1 ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=1, mF=-1 ))
+    LJFmF.append(Niveau( L=1, J=3/2, F=2, mF=-2 ))
+    LJFmF.append(Niveau( L=1, J=1/2, F=1, mF=1  ))
+    LJFmF.append(Niveau( L=1, J=1/2, F=1, mF=0  ))
+    LJFmF.append(Niveau( L=1, J=1/2, F=0, mF=0  ))
+    LJFmF.append(Niveau( L=1, J=1/2, F=1, mF=-1 ))
+    LJFmF.append(Niveau( L=0, J=1/2, F=1, mF=1  ))
+    LJFmF.append(Niveau( L=0, J=1/2, F=1, mF=0  ))
+    LJFmF.append(Niveau( L=0, J=1/2, F=0, mF=0  ))
+    LJFmF.append(Niveau( L=0, J=1/2, F=1, mF=-1 ))
+    return LJFmF
+
+def LSI_vers_LJI():
+    P = np.zeros((16,16))
+    for m,d in enumerate(LmSmLmI): # départ
+        for n,a in enumerate(LJmJmI): # arrivée
+            if d.L != a.L or d.mI != a.mI:
+                P[n,m] = 0 # Parce qu'on doit avoir les mêmes mI et L pour les deux bases
+            else:
+                P[n,m] = clebsch(j1=d.L,m1=d.mL,j2=d.S,m2=d.mS,J=a.J,M=a.mJ)
+    return P
+            
+def LJI_vers_LJF():
+    P = np.zeros((16,16))
+    for m,d in enumerate(LJmJmI): # départ
+        for n,a in enumerate(LJFmF): # arrivée
+            if d.L != a.L or d.J != a.J:
+                P[n,m] = 0 # Parce qu'on doit avoir les mêmes J et L pour les deux bases
+            else:
+                P[n,m] = clebsch(j1=d.J,m1=d.mJ,j2=d.I,m2=d.mI,J=a.F,M=a.mF)
+    return P
+    
+def clebsch(j1,m1,j2,m2,J,M):
+    return (-1)**(j1-j2+M)*np.sqrt(2*J+1)*wigner3j(j1,j2,J,m1,m2,-M)
+    
+def wigner3j(j1,j2,j3,m1,m2,m3):
+    t1 = j2 - m1 - j3
+    t2 = j1 + m2 - j3
+    t3 = j1 + j2 - j3
+    t4 = j1 - m1
+    t5 = j2 + m2
+    tmin = max(0, max(t1,t2))
+    tmax = min(t3, min(t4,t5))
+    tvec = np.arange(tmin,tmax+1,1)
+    wigner = 0
+    for t in tvec:
+        wigner += (-1)**t / ( factorial(t) * factorial(t-t1) * factorial(t-t2) * factorial(t3-t) * factorial(t4-t) * factorial(t5-t) )
+    return wigner * (-1)**(j1-j2-m3) * np.sqrt( factorial(j1+j2-j3) * factorial(j1-j2+j3) * factorial(-j1+j2+j3) / factorial(j1+j2+j3+1) * factorial(j1+m1) * factorial(j1-m1) * factorial(j2+m2) * factorial(j2-m2) * factorial(j3+m3) * factorial(j3-m3) )
+    
